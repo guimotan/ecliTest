84587671df585ddcc029a34d3fce66c909d48ba1
guimotan <48192764+guimotan@users.noreply.github.com>
2019-03-04 14:06:49 +0800

    cff

--- test/src/test/Test.java
+++ test/src/test/Test.java
@@ -5,7 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("aabb");
 		System.out.println("bbbb");
-		System.out.println("cccc");
+		System.out.println("ccff");
 		
 	}
 
